91b9aa94186bdf1f7d0ce9d98fc1f701f5c48cd6
Tawfique Mohammed <tawfiquemt17@gmail.com>
2022-03-01 15:30:16 +0600

    First part fetch done

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-function loadMobiles() {
-    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+const loadMobiles = () => {
+    const searchText = document.getElementById("search-box").value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    console.log(url);
+    fetch(url)
         .then(res => res.json())
-        .then(data => displayMobiles(data));
+        .then(data => displayMobiles(data.data));
 }
-function displayMobiles(data) {
-    const div = document.getElementById('search-button');
-    for (const mobile of data) {
-        console.log(mobile)
+const displayMobiles = (mobiles) => {
+    // const div = document.getElementById('search-button');
+    // console.log(mobiles);
+    for (const mobile of mobiles) {
+        // console.log(mobile);
+        const parent = document.getElementById('mobile-container');
+        const div = document.createElement('div');
+        div.innerHTML = `<div class="card border p-5">
+        <div class="pro-pic ">
+            <img class="w-30" src="${mobile.image}" alt="">
+        </div>
+        <h4>Phone Name: ${mobile.phone_name}</h4>
+        <h5>Brand: ${mobile.brand}</h5>
+        <p></p>
+        <div class="all-button">
+            <button onclick="details('${mobile.slug}')" class="btn btn-success">Details</button>
+        </div>
+    </div>`
+        parent.appendChild(div);
     }
+}
+
+const details = (slug) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+    console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
 }
